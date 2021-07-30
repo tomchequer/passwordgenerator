b054c07216a8b1c3237c04b00eaf364a0c860bf8
antonio <antoniochequer@gmail.com>
2021-07-30 16:37:26 -0300

    now i created a completly random password, where even the order is randomized.

--- passwordgen.py
+++ passwordgen.py
@@ -35,26 +35,52 @@ howmanynumbers = int(input("How many numbers do you want in your password?\n"))
 
 
 
-password = ""
+#password = ""
 #simplifiedpassowrd 
 #here i created a little algoritim in order to create a simple password.
 #It works building a random password like (randomletters + randomnumbers + randomsymbols)
 #but it's not 100% random, 'cause the order of those characters is not randomized.
 #but anyway is better than your regular 1234 password.
 
+#for i in range(1, howmanyletters + 1):
+  #randomletters = random.choice(letters)
+  #password += str(randomletters)
+
+#for i in range (1, (howmanynumbers + 1)):
+  #randomnumbers = random.choice(numbers)
+  #password += str(randomnumbers)
+  
+#for i in range (1, howmanysymbols + 1):
+  #randomsymbols = random.choice(symbols)
+  #password += (randomsymbols)
+
+#print(f'Your simplified password is {password}')
+
+
+
+#hardtocrackpassword
+#now i created a harder to crack password
+#which all characters and them order is completly randomized
+#making it almost impossible to crack
+
+password_list = []
 for i in range(1, howmanyletters + 1):
-  randomletters = random.choice(letters)
-  password += str(randomletters)
+  password_list.append(str(random.choice(letters)))
 
 for i in range (1, (howmanynumbers + 1)):
-  randomnumbers = random.choice(numbers)
-  password += str(randomnumbers)
+  password_list.append(str(random.choice(numbers)))
   
 for i in range (1, howmanysymbols + 1):
-  randomsymbols = random.choice(symbols)
-  password += (randomsymbols)
+  password_list.append((random.choice(symbols)))
 
-print(f'Your simplified password is {password}')
-
+random.shuffle(password_list)
 
 
+#password = ''.join(password_list)
+#i could use this method, but im using another one for practicing.
+
+password = ''
+for i in password_list:
+  password = password + str(i)
+
+print(f'Your hard to crack password is {password}')
